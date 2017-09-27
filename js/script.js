13e3c3d084bec1a5eea7fd24228a1120a7b1aa82
Marcus Ibraim <big_marcolino@hotmail.com>
2017-09-27 10:11:55 -0300

    Add spaces and remove some curly brackets

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ var timeS = 60;
 var timetoOver;
 var score = 0;
 
-$(document).ready(function(){
-	$("a[rel=modalExit]").click( function(ev){
+$(document).ready(function() {
+	$("a[rel=modalExit]").click( function(ev) {
 		ev.preventDefault();
 
 		var id = $(this).attr("href");
 		var alturaTela = $(document).height();
 		var larguraTela = $(window).width();
 
-		$('#mascara').css({'width':larguraTela,'height':alturaTela});
+		$('#mascara').css({'width':larguraTela, 'height':alturaTela});
 		$('#mascara').fadeIn(1000);
 		$('#mascara').fadeTo("slow",0.8);
 
@@ -24,25 +24,25 @@ $(document).ready(function(){
 		$(id).show();  
 	});
 
-	$("#mascara").click( function(){
+	$("#mascara").click( function() {
 		$(this).hide();
 		$("#modalExit").hide();
 	});
 });
 
-$("#yes").click(function(){
+$("#yes").click(function() {
 	window.close();
 });
 
-$("#no").click(function(){
+$("#no").click(function() {
 	$(document).ready( function() { $("#modalExit").css("display","none"); } );
 	$(document).ready( function() { $("#mascara").css("display","none"); } );
 });
 
 
 //Script para game.html
-$(document).ready(function(){
-	$("a[rel=modalPause]").click( function(ev){
+$(document).ready(function() {
+	$("a[rel=modalPause]").click( function(ev) {
 		ev.preventDefault();
 		
 		paused = true;
@@ -62,32 +62,30 @@ $(document).ready(function(){
 		$(id).show();  
 	});
 
-	$("#mascara").click( function(){
+	$("#mascara").click( function() {
 		$(this).hide();
 		$("#modalPause").hide();
 	});
 });
 
-function confirmExitToMenu(){
+function confirmExitToMenu() {
 	var r = confirm("Are you sure?");
 	
-	if (r==true){
+	if (r==true)
 		location.href="index.html"
-	}
 }
 
-function confirmLeaveGame(){
+function confirmLeaveGame() {
 	var r = confirm("Are you sure?");
 	
-	if (r==true){
+	if (r==true)
 		window.close();
-	}
 }
 
-$("#resume, #mascara").click(function(){
+$("#resume, #mascara").click(function() {
 	$(document).ready( function() { $("#modalPause, #modalExit").css("display","none"); } );
 	$(document).ready( function() { $("#mascara").css("display","none"); } );
-	timetoOver = setTimeout(function(){	
+	timetoOver = setTimeout(function() {	
 		confirmGameOver(score);
 		game.stop();
 	}, timeS*1000 + 500);
@@ -95,20 +93,20 @@ $("#resume, #mascara").click(function(){
 	game.resume();
 });
 
-$("#exitToMenu").click(function(){
+$("#exitToMenu").click(function() {
 	confirmExitToMenu();
 });
 
-$("#leaveGame").click(function(){
+$("#leaveGame").click(function() {
 	confirmLeaveGame();
 });
 
-$(document).ready(function(){
+$(document).ready(function() {
 	$("#tips1").fadeIn(1500);
 	$("#tips2").delay(1600).fadeIn(1500);
 	$("#go").delay(3100).fadeIn(200);
 });
 
-$("#pageTips").click(function(){
+$("#pageTips").click(function() {
 	location.href = "game.html";
 });
